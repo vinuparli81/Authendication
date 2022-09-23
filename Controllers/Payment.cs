3b52d56e473d5f8263d694b38c2bda9c89ea9f85
Vinod Kumar A V <vinodkumar_v@cms.co.in>
2022-09-23 18:09:03 +0530

    Payment

---
+++ Controllers/Payment.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Payment.paymentmodel;
+
+namespace Payment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Payment : ControllerBase
+    {
+        [HttpPost]
+        [Route("Payments")]
+        public IActionResult Payments(creditcarddetails _CreditcardDetails)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                return Ok("Payment Successfully!");
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+       
+    }
+}
---
+++ paymentmodel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payment
+{
+    public class paymentmodel
+    {
+        public class creditcarddetails
+        {
+            public string CardholderName { get; set; }
+            public string CardNumber { get; set; }
+            public decimal Amount { get; set; }
+        }
+    }
+}
